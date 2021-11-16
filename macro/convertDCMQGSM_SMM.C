4058957bb20e869a02d780a0397ac4f75c6027fc
Oleg Golosov <oleg.golosov@gmail.com>
2021-11-16 18:29:04 +0300

    update DCM, filter, add SMASH macros

--- macro/convertDCMQGSM_SMM.C
+++ macro/convertDCMQGSM_SMM.C
@@ -1,193 +1,372 @@
-#include "TString.h"
 #include "TFile.h"
-#include "TTree.h"
 #include "TMath.h"
+#include "TString.h"
+#include "TTree.h"
+#include "TError.h"
 
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-int getPdgType (int iBaryon, int iCharge, int iStrangeness = 0)
+int verbosity = kError;
+
+int getPdgCode (int baryonNr, int charge, int strangeness = 0)
 {
-  int pdgType = abs (iStrangeness) * 1e7 + abs (iBaryon) * 10 + abs (iCharge) * 1e4 + 1e9;
-  if (iStrangeness != 0) pdgType *= -abs (iStrangeness) / iStrangeness;
-  return pdgType;
+  int pdgCode=0;
+  if (baryonNr == 1 && charge == 1) pdgCode = 2212;
+  else if (baryonNr == 1 && charge == 0) pdgCode = 2112;
+  else if (baryonNr == -1 && charge == -1) pdgCode = -2212;
+  else if (baryonNr == 0 && charge == 0) pdgCode = 22;
+  else if (baryonNr == -1 && charge == 0) pdgCode = -2112;
+  else 
+  {
+    pdgCode = abs (strangeness) * 1e7 + abs (baryonNr) * 10 + abs (charge) * 1e4 + 1e9;
+    if (strangeness != 0) pdgCode *= -abs (strangeness) / strangeness;
+  }
+  if (verbosity <= kInfo) cout << "getPdgCode: " << pdgCode << endl;
+  return pdgCode;
 }
 
-void convertDCMQGSM_SMM (TString inFile  = "CAS-SMM-evt.out", TString outFile = "test", int nEvents = 5000, int splitFactor = 1)
+int getIonZ(int pdgCode) { return (pdgCode % 10000000) / 10000; }
+
+int getIonS(int pdgCode) { return (pdgCode % (10 * 10000000)) / 10000000; }
+
+int getIonA(int pdgCode) { return (pdgCode % 10000) / 10; }
+
+float getMass(int pdgCode)
 {
-  gSystem->Load ("libMcIniData.so");
-  const float mProton = 0.938272029;
-  const float mBaryon = 0.931494;
-  const char* generator = "DCMQGSM-SMM";
-  const char* comment = "";
-  int aProj;
-  int zProj;
-  double pProj;
-  double T0;
-  double sqrtS;
-  int aTarg;
-  int zTarg;
-  double pTarg;
-  double bMin =  0;
-  double bMax =  20.0;
-  int bWeight =  0;
-  double phiMin = 0;
-  double phiMax = 2 * TMath::Pi ();
-  double sigma = 0;
-  int eventsPerFile = nEvents / (float) splitFactor;
+  float mBaryon=0.931494, mass;
+  if (abs(pdgCode) > 1000000000)
+  {
+    mass = mBaryon*getIonA(pdgCode);
+  }
+  else 
+  {
+    TParticlePDG *p=TDatabasePDG::Instance()->GetParticle(pdgCode);
+    if (p)
+      mass = p->Mass();
+    else 
+    {
+      cout << "Unknown PDG code: " << pdgCode << endl;
+      mass = -999.;
+    }
+  }
+  if (verbosity <= kInfo) cout << "getMass: " << mass << endl;
+  return mass;
+}
 
-  URun *header;
-  UEvent *event = new UEvent;
-  EventInitialState *iniState = new EventInitialState;
+unsigned int nWordsIn(const std::string &line)
+{
+  std::stringstream stream(line);
+  std::string word;
+  unsigned int words = 0;
+  while(stream >> word)
+  {
+    ++words;
+  }
+  return words;
+}
 
-  int filenum = 0;
-  int eventNr, nTracks, pid, iBaryon, iCharge, iStrangeness;
-  float b, bx, by, px, py, pz, e;
-  float mass;
-  float ExEnergy;
-  int pdgType;
-  int nEvent=0;
-  bool skipFlag;
-  double phi = 0;
+bool openFile(ifstream &inputFile, TString inputFileName)
+{
+  inputFile.open(inputFileName);
+  if(!inputFile)
+  {
+    printf("File does not exist\n");
+    return false;
+  }
+  cout << Form("--> Successfully opened file") << endl << endl;
+  return true;
+}
+
+URun *makeRunHeader(ifstream &inputFile)
+{
+  float mProton = TDatabasePDG::Instance()->GetParticle(2212)->Mass();
+  string line, generator = "DCM-QGSM-SMM";
+  const char *comment = "";
+  int aProj, zProj, aTarg, zTarg;
+  float eBeam, sqrtS, bWeight = 1, bMin = 0, bMax = 20.0, phiMin = 0, phiMax = 2 * TMath::Pi(), sigma = 0, nEvents = 0;
+
+  getline(inputFile, line);  // generator and system info
+  if(nWordsIn(line) != 6)
+  {
+    cout << "FATAL: wrong number of words in run info:" << endl << line << endl;
+    return nullptr;
+  }
+  stringstream(line) >> aProj >> zProj >> aTarg >> zTarg >> eBeam >> sqrtS;
+
+  if (verbosity <= kInfo) 
+    cout << "\t" << aProj << "\t" << zProj << "\t" << aTarg << "\t" << zTarg << "\t" << eBeam << "\t" << sqrtS << endl;
+
+  float pProj = sqrt(0.5 * mProton * eBeam);
+  float pTarg = -pProj;
+
+  URun *header = new URun(generator.c_str(), comment, aProj, zProj, pProj, aTarg, zTarg, pTarg, bMin, bMax, bWeight,
+                          phiMin, phiMax, sigma, nEvents);
+  header->Print();
+  return header;
+}
+
+void treatConcatenations(string &line)
+{
+  TString tempLine(line);
+  tempLine.ReplaceAll("-", " -");
+  tempLine.ReplaceAll("E -", "E-");
+  line=tempLine;
+}
+
+bool fillEventInfo(string &line, UEvent *event, EventInitialState *iniState, int eventNr)
+{
+  treatConcatenations(line);
+  if(nWordsIn(line) != 4)
+  {
+    cout << "FATAL: Wrong number of variables in event info:" << endl << line << endl;
+    return false;
+  }
+  int eventId;
+  float b, bx, by;
+  stringstream(line) >> eventId >> b >> bx >> by;
+  if (verbosity <= kInfo)
+  {
+    cout << line << endl;
+    cout << eventId << "\t" << b << "\t" << bx << "\t" << by << endl;
+  }
   int nes = 0;
   int stepNr = 0;
   double stepT = 0;
-  int status = 0;
-  int parent = 0;
-  int parentDecay = 0;
-  int mate = 0;
-  int decay = 0;
-  int child[2] = {0, 0};
-  double x = 0;
-  double y = 0;
-  double z;
-  double t;
-  double weight = 1;
+  float phi = atan2(by, bx);
+  event->SetParameters(eventNr, b, phi, nes, stepNr, stepT);
+  iniState->setId(eventNr);
+  return true;
+}
 
-  ifstream *InputFile = new ifstream (inFile);
+bool addResidual(ifstream &inputFile, EventInitialState *iniState, int resType, bool &skipFlag)
+{
+  string line;
+  getline(inputFile, line);
+//  treatConcatenations(line);
+  if(nWordsIn(line) != 6)
+  {
+    cout << "FATAL: Wrong number of variables in residual info:" << endl << line << endl;
+    return false;
+  }
+  int baryonNr, charge, pdgCode;
+  float eExcitation, x = 0, y = 0, z, t=resType, px, py, pz, m;
+  stringstream(line) >> baryonNr >> charge >> eExcitation >> px >> py >> pz;
+  if(verbosity <= kInfo)
+  {
+    cout << "addResidual:\n";
+    cout << line << endl;
+    cout << baryonNr << "\t" << charge << "\t" << eExcitation << "\t" << px
+         << "\t" << py << "\t" << pz << endl;
+  }
+  z = eExcitation; // just to store it somewhere
+  pdgCode = getPdgCode(baryonNr, charge);
+  m = getMass(pdgCode);
+  if (t < 0. && pz > 0.) skipFlag = true;  // exclude events with positive Pz of target spectators
+
+  TLorentzVector position(x, y, z, t);
+  TLorentzVector momentum;
+  momentum.SetXYZM(px, py, pz, m);
+  Nucleon nucleon;
+  nucleon.setId(resType);
+  nucleon.setPdgId(pdgCode);
+  nucleon.setPosition(position);
+  nucleon.setMomentum(momentum);
+  iniState->addNucleon(nucleon);
+
+  return true;
+}
+
+bool addSpectator(ifstream &inputFile, UEvent *event, int spectIndex, int spectType)
+{
+  string line;
+  getline(inputFile, line);
+  treatConcatenations(line);
+  if(nWordsIn(line) != 6)
+  {
+    cout << "FATAL: Wrong number of variables in spectator info:"<< endl << line << endl;
+    return false;
+  }
+  int charge, baryonNr, pdgCode=0;
+  int status = 0, parent = -1, parentDecay = 0, mate = 0, decay = 0, child[2] = {0, 0};
+  double eExcitation, x = 0., y = 0., z, t = spectType, weight = 1., px, py, pz, e, m;
+  stringstream(line) >> baryonNr >> charge >> eExcitation >> px >> py >> pz;
+  if(verbosity <= kInfo)
+  {
+    cout << "addSpectator:\n";
+    cout << line << endl;
+    cout << baryonNr << "\t" << charge << "\t" << eExcitation << "\t" << px
+         << "\t" << py << "\t" << pz << endl;
+  }
+ 
+  z=eExcitation; // just to store it somewhere 
+  pdgCode = getPdgCode (baryonNr, charge);
+  if (pdgCode==0)
+  {
+    cout << "FATAL: Undefined spectator:" << endl << line << endl;
+    return false;
+  }
+  m=getMass(pdgCode);
+  if (m < 0.) 
+    return false;
+  e = sqrt(px * px + py * py + pz * pz + m * m);
+
+  event->AddParticle(spectIndex, pdgCode, status, parent, parentDecay, mate, decay, child, px, py, pz, e, x, y, z, t, weight);
+  return true;
+}
+
+bool addParticle(ifstream &inputFile, UEvent *event, int partIndex)
+{
+  string line;
+  getline(inputFile, line);
+  treatConcatenations(line);
+  if(nWordsIn(line) != 8)
+  {
+    cout << "FATAL: Wrong number of variables in particle info! Aborting!\n";
+    cout << line << endl;
+    return false;
+  }
+  int charge, leptonNr, baryonNr, strangeness, pdgCode;
+  int status = 0, parent = -1, parentDecay = 0, mate = 0, decay = 0, child[2] = {0, 0};
+  double x = 0., y = 0., z, t = 0, weight = 1., px, py, pz, e, m;
+  stringstream(line) >> pdgCode >> baryonNr >> charge >> strangeness >> px >> py >> pz >> m;
+  if(verbosity <= kInfo)
+  {
+    cout << "addParticle:\n";
+    cout << line << endl;
+    cout << pdgCode << "\t" << baryonNr << "\t" << charge << "\t" << strangeness << "\t" << px
+         << "\t" << py << "\t" << pz << "\t" << m << "\t" << e << endl;
+  }
+  e = sqrt(px * px + py * py + pz * pz + m * m);
+  pdgCode = TDatabasePDG::Instance()->ConvertIsajetToPdg(pdgCode);
+  if (pdgCode == 3122 && m > 1.120)
+  {
+    if (verbosity <= kWarning)
+      cout << "WARNING: changing PDG code from 3122 (lambda) to 3212 (sigma) for a particle with mass = " << m << endl;
+    pdgCode = 3212;
+    status = 1;
+  }
+  event->AddParticle(partIndex, pdgCode, status, parent, parentDecay, mate, decay, child, px, py, pz, e, x, y, z, t,
+                     weight);
+  return true;
+}
+
+void convertDCMQGSM_SMM(TString inputFileName = "CAS-SMM-evt.out",
+                   TString outFile = "test",
+                   int nEvents = 5000,
+                   int splitFactor = 1)
+{
+  gSystem->Load("libMcIniData.so");
+  const float mProton = TDatabasePDG::Instance()->GetParticle(2212)->Mass();
+  int eventsPerFile = nEvents / (float)splitFactor;
+
+  int filenum = 0;
+
+  ifstream inputFile;
+  if(!openFile(inputFile, inputFileName))
+    return;
+  URun *header = makeRunHeader(inputFile);
+  if(!header)
+    return;
+  TString generator;
+  header->GetGenerator(generator);
+  UEvent *event = new UEvent;
+  EventInitialState *iniState = new EventInitialState;
   TTree *tree = NULL;
   TFile *f = NULL;
-  TDatabasePDG db;
+  string line;
+  int nEvent=0;
   
-  *InputFile >> aProj >> zProj >> aTarg >> zTarg >> T0 >> sqrtS;
-//  cout << "\t" << aProj << "\t" <<  zProj << "\t" <<  aTarg << "\t" <<  zTarg << "\t" <<  T0 << "\t" <<  sqrtS << endl;
-  double E = 0.5 * sqrtS;
-  pProj = sqrt(E*E-mProton*mProton);
-  pTarg = - pProj;
-  
-  while (*InputFile >> eventNr >> b >> bx >> by)
+  while(getline(inputFile, line) && nEvent < nEvents)
   {
-//    cout << eventNr << "\t" << b << "\t" << bx << "\t" << by << endl;
-//    cout << nEvent << endl;
-    if (nEvent % eventsPerFile == 0)
+    event->Clear();
+    iniState->Clear();
+    if(nEvent % eventsPerFile == 0)
     {
-      if (tree) tree -> Write ();
-      if (f) f -> Close ();
-      f = new TFile ( Form ("%s_%d.root", outFile.Data (), filenum), "recreate");
-      tree = new TTree ("events", generator);
-      header = new URun(generator, comment, aProj, zProj, pProj, aTarg, zTarg, pTarg, bMin, bMax, bWeight, phiMin, phiMax, sigma, eventsPerFile);
-      header->Write();
+      if(f)
+      {
+        header->SetNEvents(nEvent);
+        header->Write();
+        tree->Write();
+        f->Close();
+        nEvent = 0;
+      }
+      f = new TFile(Form("%s_%d.root", outFile.Data(), filenum), "recreate");
+      tree = new TTree("events", generator);
       tree->Branch("event", "UEvent", event);
       tree->Branch("iniState", "EventInitialState", iniState);
       filenum++;
     }
 
+    if((nEvent % 50) == 0)
+      cout << "Event # " << nEvent << "... \r" << flush;
+
     nEvent++;
-    event -> Clear();
-    iniState -> Clear();
-    if ( (nEvent % 50) == 0 ) cout << "Event # " << nEvent << "... \r" << flush;
-
-    phi = TMath::ATan2 (by, bx);
-
-    skipFlag = false;
-    event -> SetParameters (eventNr, b, phi, nes, stepNr, stepT);
-    iniState -> setId(eventNr);
+    if (!fillEventInfo(line, event, iniState, nEvent))
+      return;
+    bool skipFlag=false;
     int index = 0;
+    for(int spectType = 1; spectType >= -1; spectType-=2) // -1 for target and 1 for projectile spectators
+    {
+      if (!addResidual(inputFile, iniState, spectType, skipFlag)) 
+        return;
+    }
+    for(int spectType = 1; spectType >= -1; spectType-=2) // -1 for target and 1 for projectile spectators
+    {
+      getline(inputFile, line);
+      if (nWordsIn(line) != 1)
+      {
+        cout << "FATAL: Wrong number of words in number of spectators line:" << endl << line << endl;
+        return;
+      }
+      int nSpect;
+      stringstream(line) >> nSpect;
+      if (verbosity <= kInfo) 
+      {
+        cout << "nSpect:\n";
+        cout << line << endl;
+        cout << nSpect << endl;
+      }
+      for(int iSpect = 0; iSpect < nSpect; iSpect++)
+      {
+        if (!addSpectator(inputFile, event, index, spectType))
+          return;
+        else index++;
+      }
+    }
     
-    for (int spectType = 0; spectType < 2; spectType++)
+    getline(inputFile, line);
+    if (nWordsIn(line) != 1)
     {
-      *InputFile >> iBaryon >> iCharge >> ExEnergy >> px >> py >> pz;
-//      cout << iBaryon << "\t" << iCharge << "\t" << ExEnergy << "\t" << px << "\t" << py << "\t" << pz << "\t" << endl;
-      z = ExEnergy;
-      t = 1 - 2 * spectType; // -1 for target spectators, +1 for projectile
-      pdgType = getPdgType (iBaryon, iCharge);
-      TLorentzVector momentum, position;
-      momentum.SetXYZM (px, py, pz, iBaryon * mBaryon);
-      position.SetXYZT (0, 0, z, t);
-      vector <unsigned short> collisions;
-      Nucleon nucleon (spectType, pdgType, momentum, position, kInelasticWithInitialNucleon, collisions);
-      iniState -> addNucleon (nucleon);
-      if (t < 0. && pz > 0.) skipFlag = true;  // exclude events with positive Pz of target spectators
+      cout << "FATAL: Wrong number of words in number of produced line:" << endl << line << endl;
+      return;
     }
-
-    for (int spectType = 0; spectType < 2; spectType++)
+    int nProduced;
+    stringstream(line) >> nProduced; 
+    if (verbosity <= kInfo) 
     {
-      *InputFile >> nTracks;
-//      cout << nTracks << endl;
-
-      for (int iTrack = 0; iTrack < nTracks; iTrack++, index++ )
-      {
-        *InputFile >> iBaryon >> iCharge >> ExEnergy >> px >> py >> pz;
-//        cout << iBaryon << "\t" << iCharge << "\t" << ExEnergy << "\t" << px << "\t" << py << "\t" << pz << "\t" << endl;
-
-        if (iBaryon > 1 && iCharge > 0)  pdgType = getPdgType (iBaryon, iCharge);
-        else if (iBaryon == 1 && iCharge == 1) pdgType = 2212;
-        else if (iBaryon == 1 && iCharge == 0) pdgType = 2112;
-        else if (iBaryon == 0 && iCharge == 0) pdgType = 22;
-        else if (iBaryon == -1 && iCharge == -1) pdgType = -2212;
-        else if (iBaryon == -1 && iCharge == 0) pdgType = -2112;
-        else
-        {
-          cout << "Undef spectator:\t" << eventNr << "\t" << iTrack << "\n" << iBaryon << "\t" << iCharge << "\t" << iStrangeness << "\t" << px << "\t" << py << "\t" << pz << "\t" << endl;
-          iTrack ++;
-          continue;
-        }
-
-        z = ExEnergy;
-        t = 1 - 2 * spectType; // -1 for target spectators, +1 for projectile
-        weight = 1;
-        if (pdgType == 22) mass = 0.;
-        else mass = iBaryon * 0.931494;
-
-        e = TMath::Sqrt (px * px + py * py + pz * pz + mass * mass);
-
-        event -> AddParticle (index, pdgType, status, parent, parentDecay, mate, decay, child, px, py, pz, e, x, y, z, t, weight);
-      }
+      cout << "nProduced:\n";
+      cout << line << endl;
+      cout << nProduced << endl;
     }
-
-    *InputFile >> nTracks;
-//    cout << nTracks << endl;
-
-    z = 0;
-    t = 0;
-
-    for (int iTrack = 0; iTrack < nTracks; iTrack++, index++ )
+    for(int iPart = 0; iPart < nProduced; iPart++)
     {
-      *InputFile >> pid >> iBaryon >> iCharge >> iStrangeness >> px >> py >> pz >> mass;
-//      cout << pid << "\t" << iBaryon << "\t" << iCharge << "\t" << iStrangeness << "\t"<< px << "\t" << py << "\t" << pz << "\t" << mass << endl;
-      if (pid == 0)
-      {
-        pdgType = getPdgType (iBaryon, iCharge, iStrangeness);
-      }
-      else pdgType = db.ConvertIsajetToPdg(pid);
-      if (pdgType == 3122 && mass > 1.120)   // lambda with sigma mass
-      {
-        pdgType = 3212;
-        status = 1;
-      }
-      else status = 0;
-      
-      e = TMath::Sqrt(px * px + py * py + pz * pz + mass * mass);
-      event -> AddParticle (index, pdgType, status, parent, parentDecay, mate, decay, child, px, py, pz, e, x, y, z, t, weight);
+      if (!addParticle(inputFile, event, index))
+        return;
+      else index++;
     }
-
-    if (skipFlag) continue; // exclude events with positive Pz of target spectators
-    tree -> Fill ();
+    if (!skipFlag)
+      tree->Fill();
+    else if (verbosity <= kError)
+      cout << "ERROR: skipping event due to presence of target spectators with positive Pz!!!\n";
   }
 
-  InputFile -> close ();
-  tree -> Write ();
-  f -> Close ();
+  cout << "\rEvent # " << nEvent << endl;
+  inputFile.close();
+  header->SetNEvents(nEvent);
+  header->Write();
+  tree->Write();
+  f->Close();
 }
--- macro/filterUnigen.C
+++ macro/filterUnigen.C
@@ -1,39 +1,43 @@
+bool removeEvent(UEvent *event)
+{
+  if(event->GetB() > 4.) return true;
+  return false;
+}
+
 bool removeParticle(UParticle *part)
 {
   if(part->GetPdg() != 2212) return true;
   return false;
 }
 
-void filterUnigen (TString inPath="~/desktop/analysis/mc/model_root/urqmd_12.root", TString outPath="out.root", int eventsPerFile=1000)
+void filterUnigen (TString inPath="~/desktop/analysis/mc/model_root/dcmqgsm_smm_auau_pbeam12agev/dcmqgsm_1.root", TString outPath="out.root", int eventsPerFile=200)
 {
   outPath.ReplaceAll(".root","");
   TChain ch("events");
   ch.Add(inPath);
   TFile *f{nullptr};
   TTree *t{nullptr};
-  int fileNumber=0;
+  int fileNumber=1;
+  int nAcceptedEvents=0;
   auto nEvents=ch.GetEntries();
   UEvent *event{nullptr};
   ch.SetBranchAddress("event", &event);
-  ch.GetEntry(0);
   auto runInfo=(URun*)(ch.GetFile()->Get("run"));
   for (int e=0;e<nEvents;e++)
   {
-    ch.GetEntry(e);
     cout << "\r " << e << " / " << nEvents;
-    if(e % eventsPerFile == 0)
-    {
-      if(f)
-      {
-        runInfo->SetNEvents(t->GetEntries());
-        runInfo->Write();
-        t->Write();
-        f->Close();
-	fileNumber++;
-      }
+    if(nAcceptedEvents == 0)
+    {  
       f=new TFile(Form("%s_%d.root", outPath.Data(), fileNumber), "recreate");
       t=ch.CloneTree(0);
     }
+    
+    cout << "\t" << nAcceptedEvents << "\t" << eventsPerFile << "\t" << e << "\t" << nEvents << endl;    
+ 
+    ch.GetEntry(e);
+    if (removeEvent(event))
+      continue;
+    nAcceptedEvents++;
     int nParticles=event->GetNpa();
     for (int p=0;p<nParticles;p++)
     {
@@ -44,12 +48,17 @@ void filterUnigen (TString inPath="~/desktop/analysis/mc/model_root/urqmd_12.roo
         nParticles--; // new indexing - see UEvent::RemoveAt(int i)
       }
     }
-    t->Fill();   
-    event->Clear();
+    t->Fill();
+    if(nAcceptedEvents == eventsPerFile || e == nEvents-1)
+    {
+      runInfo->SetNEvents(nAcceptedEvents);
+      runInfo->Write();
+      t->Write();
+      f->Close();
+      nAcceptedEvents=0;
+      fileNumber++;
+    }
+    //event->Clear();
   }
-  runInfo->SetNEvents(t->GetEntries());
-  runInfo->Write();
-  t->Write();
-  f->Close();
   cout << endl;
 }
